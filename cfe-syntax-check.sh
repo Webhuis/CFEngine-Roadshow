b02948ca38a0241ff601d9b3ea4994533ac5776f
martin <martin@iMac-van-mieke.local>
2014-10-07 01:46:01 +0200

    CFEngine Roadshow maiden voyage

--- cfe-syntax-check.sh
+++ cfe-syntax-check.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-cd /root/mirror.webhuis.nl/roadshow/roadshow
+cd /var/lib/svn/roadshow
+
+/usr/bin/svn update
 
 /usr/bin/find . -name "*.cf" -exec /var/cfengine/bin/cf-promises {} \; | grep syntax
 
