e9401b1ab7b7e992deb2240f443dd5d221c5beca
Webhuis <martin@webhuis.nl>
2014-11-12 22:51:11 +0000

    bin op git

--- bin/rsync-masterfiles.sh
+++ bin/rsync-masterfiles.sh
@@ -2,7 +2,7 @@
 
 cd /var/lib/git/CFEngine-Roadshow
 
-/usr/bin/rsync -qaC --delete /var/lib/git/CFEngine-Roadshow/masterfiles/* /var/cfengine/masterfiles
-/usr/bin/rsync -qaC --delete /var/lib/git/CFEngine-Roadshow/dynamic_policies/* /var/cfengine/dynamic_policies
+/usr/bin/rsync -qaC --delete /var/lib/git/CFEngine-Roadshow/policies/masterfiles/* /var/cfengine/masterfiles
+/usr/bin/rsync -qaC --delete /var/lib/git/CFEngine-Roadshow/policies/dynamic_policies/* /var/cfengine/dynamic_policies
 
 cd -
