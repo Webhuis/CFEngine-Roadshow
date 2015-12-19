a6f17944488b138470d20a357e0d536bc62a2b80
martin <martin@iMac-van-mieke.local>
2015-12-19 21:26:13 +0100

    Data Python scripts for Data

---
+++ policies/dynamic_policies/bin/cf-messaged.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+
+import json
+import psycopg2
+import time
+import zmq
+
+context = zmq.Context()
+socket = context.socket(zmq.REP)
+socket.bind("tcp://*:5309")
+
+conn = psycopg2.connect(database="raw_messages_in", user="www-data", password="We8hu15iio", host="127.0.0.1", port="5432")
+
+while True:
+    #  Wait for next request from client
+    message = socket.recv()
+    cur = conn.cursor()
+    cur.execute("insert into json_in (message_in) velues ( message )");
+    conn.commit()
+#    with open("queue.txt", "a") as queue:
+#      queue.write(message + "\n")
+
+    #  Send reply back to client
+    response = "message received"
+    socket.send( response )
+conn.close()
