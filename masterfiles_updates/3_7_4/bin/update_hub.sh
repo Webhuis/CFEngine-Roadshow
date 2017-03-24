241c6ef696c979cc0da2684af094f8630311852d
Martin Simons <martin@webhuis.nl>
2017-03-24 17:45:24 +0100

    Fix roadshow bin

--- masterfiles_updates/3_7_4/bin/update_hub.sh
+++ masterfiles_updates/3_7_4/bin/update_hub.sh
@@ -24,5 +24,6 @@ rm /var/cfengine/masterfiles/services/autorun/hello.cf
 
 find /var/cfengine/masterfiles -type f -exec chmod -R 600 {}\;
 find /var/cfengine/roadshow_policies -type f -exec chmod -R 600 {}\;
+chmod +x /var/cfengine/roadshow_policies/bin/*.py
 
 cf-agent -B ${policy_hub}
