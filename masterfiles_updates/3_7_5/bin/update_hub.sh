8f410e5d376dc782cfff665bb4b30385e5fb7dd9
martin <martin@cfengine3.webhuis.nl>
2017-07-27 12:55:11 +0200

    masterfiles_updates update.cf

---
+++ masterfiles_updates/3_7_5/bin/update_hub.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+policy_dir=//var/lib/git/CFEngine-Roadshow
+policy_hub=`hostname -f`
+
+echo ${policy_hub}
+
+cd ${policy_dir}
+
+git pull
+
+/etc/init.d/cfengine3 stop
+
+rm -rf /var/cfengine/masterfiles/*
+rm -rf /var/cfengine/inputs/*
+rm -rf /var/cfengine/roadshow_policies
+
+cp -a ${policy_dir}/masterfiles/3_7_4/* /var/cfengine/masterfiles
+cp -a ${policy_dir}/masterfiles_updates/3_7_4/* /var/cfengine/masterfiles
+
+cp -a ${policy_dir}/roadshow_policies /var/cfengine
+
+rm /var/cfengine/masterfiles/cf_promises_validated
+rm /var/cfengine/masterfiles/services/autorun/hello.cf
+
+find /var/cfengine/masterfiles -type f -exec chmod -R 600 {} \;
+find /var/cfengine/roadshow_policies -type f -exec chmod -R 600 {} \;
+chmod +x /var/cfengine/roadshow_policies/bin/*.py
+
+cf-agent -B ${policy_hub}
