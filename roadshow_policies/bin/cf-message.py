70c5fa41ac41bdb70b3f4fe9cc20225f50e12ba9
Martin Simons <martin@webhuis.nl>
2023-10-01 00:11:55 +0200

    Reviving Data

--- roadshow_policies/bin/cf-message.py
+++ roadshow_policies/bin/cf-message.py
@@ -3,14 +3,19 @@
 import sys
 import zmq as zmq
 
+cf_message_log = open('/var/log/cf_message_log', 'a+')
+
 context = zmq.Context()
 
 #  Socket to talk to server
 #print("Connecting to server...")
 socket = context.socket(zmq.REQ)
 socket.setsockopt(zmq.LINGER, 0)
-#socket.connect("tcp://data.webhuis.nl:5309")
-socket.connect("tcp://10.68.171.110:5309")
+try:
+  socket.connect("tcp://10.68.171.110:5309")
+  cf_message_log.write('Socket connect is Ok!\n')
+except Exception as e:
+  cf_message_log.write('Fout in socket connect!\n')
 
 message = ''
 # sys.argv[0] is the program filename, slice it off
@@ -18,10 +23,19 @@ for element in sys.argv[1:]:
   message = message + element
 
 #print("Sending request %s ..." % message )
-socket.send( message )
+try:
+  socket.send( message )
+  cf_message_log.write(message + '\n' + 'Query is Ok!\n')
+except Exception as e:
+  cf_message_log.write(message + '\n' + 'Fout in execute query!\n')
 
 poller = zmq.Poller()
-poller.register(socket, zmq.POLLIN)
+try:
+  poller.register(socket, zmq.POLLIN)
+  cf_message_log.write(response + '\n' + 'Query is Ok!\n')
+except Exception as e:
+  cf_message_log.write('Fout in execute query!' + e.args + '\n')
+
 if poller.poll(2*1000): # 10s timeout in milliseconds
   response = socket.recv()
 else:
@@ -29,4 +43,6 @@ else:
   response = ("Timeout processing auth request")
 # quit()
 
-print( response )
+cf_message_log.write( response )
+cf_message_log.close()
+
