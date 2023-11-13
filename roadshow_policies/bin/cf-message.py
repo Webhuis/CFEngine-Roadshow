6c9a91e372fada448588e0f9d0ae77f711671024
Martin Simons <martin@webhuis.nl>
2023-11-13 15:55:37 +0100

    Reactivating response from Data

--- roadshow_policies/bin/cf-message.py
+++ roadshow_policies/bin/cf-message.py
@@ -3,41 +3,44 @@
 import sys
 import zmq as zmq
 
+def message_Data(message):
+  try:
+    socket.connect("tcp://10.68.171.111:5309")
+    cf_message_log.write('Socket connect is Ok!\n')
+  except Exception as e:
+    cf_message_log.write('Error in socket connect! ' + "".join(e.args) + '\n')
+
+  # sys.argv[0] is the program filename, slice it off
+  for element in sys.argv[1:]:
+    message = message + element
+
+  b_message = bytes(message, 'utf-8')
+  try:
+    socket.send( b_message )
+    cf_message_log.write(message + '\n' + 'Query is Ok!\n')
+  except Exception as e:
+    cf_message_log.write(message + '\n' + 'Error in socket send! ' + "".join(e.args) + '\n')
+
+  try:
+    b_response = socket.recv()
+    response = b_response.decode()
+    cf_message_log.write(response + '\n' + 'Response is Ok!\n')
+    print('resonse', resonse)
+  except Exception as e:
+    cf_message_log.write(message + '\n' + 'Error in response!! ' + "".join(e.args) + '\n')
+  else:
+    response = ("Timeout processing auth request!\n")
+  return response
+
 cf_message_log = open('/var/log/cf_message_log', 'a+')
 
 context = zmq.Context()
-
-#  Socket to talk to server
-
 socket = context.socket(zmq.REQ)
 
-try:
-  socket.connect("tcp://10.68.171.111:5309")
-  cf_message_log.write('Socket connect is Ok!\n')
-except Exception as e:
-  cf_message_log.write('Error in socket connect! ' + "".join(e.args) + '\n')
-
-message = ''
-# sys.argv[0] is the program filename, slice it off
-for element in sys.argv[1:]:
-  message = message + element
-
-b_message = bytes(message, 'utf-8')
-try:
-  socket.send( b_message )
-  cf_message_log.write(message + '\n' + 'Query is Ok!\n')
-except Exception as e:
-  cf_message_log.write(message + '\n' + 'Error in socket send! ' + "".join(e.args) + '\n')
-
-try:
-  b_response = socket.recv()
-  response = b_response.decode()
-  cf_message_log.write(response + '\n' + 'Response is Ok!\n')
-except Exception as e:
-  cf_message_log.write(message + '\n' + 'Error in response!! ' + "".join(e.args) + '\n')
-else:
-  response = ("Timeout processing auth request!\n")
+message = sys.argv[1:]
+print('message-in', message)
+response = message_Data(message)
 
 cf_message_log.close()
 
-
+return response
