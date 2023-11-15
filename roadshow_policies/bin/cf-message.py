0b7af75f328c8f2c6395605f28921eabd8dff509
Martin Simons <martin@webhuis.nl>
2023-11-15 21:51:16 +0100

    cf-message.py error messages

--- roadshow_policies/bin/cf-message.py
+++ roadshow_policies/bin/cf-message.py
@@ -9,12 +9,8 @@ def message_Data(message):
     socket.setsockopt(zmq.LINGER, 0)
     cf_message_log.write('Socket connect is Ok!\n')
   except Exception as e:
-    cf_message_log.write('Error in socket connect! ' + "".join(e.args) + '\n')
-    response = ('Error in socket connect! ' + "".join(e.args))
-
-  # sys.argv[0] is the program filename, slice it off
-  #for element in sys.argv[1:]:
-  #  message = message + element
+    cf_message_log.write('Error in socket connect!\n{}'.format(e.args))
+    response = ('Error in socket connect!\n{}'.format(e.args))
 
   poller = zmq.Poller()
   poller.register(socket, zmq.POLLIN)
@@ -22,27 +18,23 @@ def message_Data(message):
   cf_message_log.write('b_message in\n{}'.format(b_message))
   try:
     socket.send( b_message )
-    if poller.poll(10000):
+    if poller.poll(1000):
       cf_message_log.write('Message sent!\n')
-      message_sent = True
     else:
       cf_message_log.write(message + '\n' + 'Timeout in send!! ')
-      message_sent = False
       response = ('Error in send!')
   except Exception as e:
-    cf_message_log.write('Error in socket send! ' + "".join(e.args) + '\n')
-    response = ('Error in socket send! ' + "".join(e.args))
+    cf_message_log.write('Error in socket send!\n{} '.format(e.args))
+    response = ('Error in socket send!\n{}'.format(e.args))
 
   try:
     b_response = socket.recv()
-    #print(b_response)
     response = b_response.decode()
   except Exception as e:
-    cf_message_log.write(message + '\n' + 'Error in response!! ' + "".join(e.args) + '\n')
-    response = ('Error in response! ' + "".join(e.args))
+    cf_message_log.write(message + '\n' + 'Error in response!! '.format(e.args))
+    response = ('Error in response!\n{}'.format(e.args))
   else:
-    cf_message_log.write(response + '\n' + 'Response is Ok!\n')
-    print(response)
+    cf_message_log.write(response + '\n' + 'Response is Ok!\n{}'.format(e.args))
   return response
 
 cf_message_log = open('/var/log/cf_message_log', 'a+')
