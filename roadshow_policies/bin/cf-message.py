9f30ff448071f9edfcc2b9113c49a5738b7eb204
Martin Simons <martin@webhuis.nl>
2023-11-16 21:44:51 +0100

    restore old cf-message.py

--- roadshow_policies/bin/cf-message.py
+++ roadshow_policies/bin/cf-message.py
@@ -1,50 +1,40 @@
 #!/usr/bin/env python3
-
 import sys
 import zmq as zmq
-
 def message_Data(message):
   try:
     socket.connect("tcp://10.68.171.111:5309")
-    socket.setsockopt(zmq.LINGER, 0)
     cf_message_log.write('Socket connect is Ok!\n')
   except Exception as e:
-    cf_message_log.write('Error in socket connect!\n{}'.format(e.args))
-    response = ('Error in socket connect!\n{}'.format(e.args))
-
-  poller = zmq.Poller()
-  poller.register(socket, zmq.POLLIN)
+    cf_message_log.write('Error in socket connect! ' + "".join(e.args) + '\n')
+  # sys.argv[0] is the program filename, slice it off
+  #for element in sys.argv[1:]:
+  #  message = message + element
   b_message = message[0].encode('utf-8')
   cf_message_log.write('b_message in\n{}'.format(b_message))
   try:
     socket.send( b_message )
     cf_message_log.write('Message sent!\n')
   except Exception as e:
-    cf_message_log.write('Error in socket send!\n{} '.format(e.args))
-    response = ('Error in socket send!\n{}'.format(e.args))
+    cf_message_log.write('Error in socket send! ' + "".join(e.args) + '\n')
 
   try:
     b_response = socket.recv()
+    cf_message_log.write(b_response + '\n' + 'Query is Ok!\n')
+    #print(b_response)
     response = b_response.decode()
     cf_message_log.write(response + '\n' + 'Response is Ok!\n')
+    print('response', response)
   except Exception as e:
-    cf_message_log.write(message + '\n' + 'Error in response!! '.format(e.args))
-    response = ('Error in response!\n{}'.format(e.args))
-
-  cf_message_log.write(response + '\n' + 'Inhoud response voor return!\n')
-
+    cf_message_log.write(message + '\n' + 'Error in response!! ' + "".join(e.args) + '\n')
+  else:
+    response = ("Timeout processing auth request!\n")
   return response
-
 cf_message_log = open('/var/log/cf_message_log', 'a+')
-
 context = zmq.Context()
 socket = context.socket(zmq.REQ)
-
 message = sys.argv[1:]
 cf_message_log.write('Message in\n{}'.format(message))
 #print('message-in', message)
-
 response = message_Data(message)
-cf_message_log.write(response + '\n' + 'Inhoud response na function call.\n')
 cf_message_log.close()
-
