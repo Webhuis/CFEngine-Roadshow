7750fdb024a6e1fbbe4940a0217efbe74d1fc18c
Martin Simons <martin@webhuis.nl>
2025-12-16 20:12:29 +0100

    Roadshow update:x

--- masterfiles_updates/3_7_5/bin/update_hub.sh
+++ masterfiles_updates/3_7_5/bin/update_hub.sh
@@ -26,6 +26,8 @@ cp -rL ${policy_dir}/roadshow_policies /var/cfengine
 rm /var/cfengine/masterfiles/cf_promises_validated
 rm /var/cfengine/masterfiles/services/autorun/hello.cf
 
+cp -p /var/cfengine/inputs/cf_promises_release_id /var/cfengine/masterfiles
+
 find /var/cfengine/masterfiles -type f -exec chmod -R 600 {} \;
 find /var/cfengine/roadshow_policies -type f -exec chmod -R 600 {} \;
 chmod +x /var/cfengine/roadshow_policies/bin/*.py
